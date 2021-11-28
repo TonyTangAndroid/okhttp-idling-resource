bf5d73615f363756e9ab6f323543bb9c4b0c65f4
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 00:32:27 -0800

    Add EspressoApp as android unit test code to fix the unit test

---
+++ app/src/androidTest/java/com/demo/app/EspressoApp.java
@@ -0,0 +1,14 @@
+package com.demo.app;
+
+import androidx.test.espresso.IdlingRegistry;
+import com.jakewharton.espresso.OkHttp3IdlingResource;
+
+public class EspressoApp extends App {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    IdlingRegistry.getInstance().register(OkHttp3IdlingResource.create("OkHttp", okhttpClientInstance()));
+  }
+
+}
