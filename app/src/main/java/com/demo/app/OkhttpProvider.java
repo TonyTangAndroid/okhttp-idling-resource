129933277360a5f37b03019953f56b6f77466208
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-27 23:45:21 -0800

    Confirm that the Okhttp IdleResource does not solve the problem due to the extra delay on rendering UI.

--- app/src/main/java/com/demo/app/OkhttpProvider.java
+++ app/src/main/java/com/demo/app/OkhttpProvider.java
@@ -1,19 +1,30 @@
 package com.demo.app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.test.espresso.IdlingResource;
+import com.jakewharton.espresso.OkHttp3IdlingResource;
 import com.simplemented.okdelay.DelayInterceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.OkHttpClient.Builder;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OkhttpProvider {
 
+  public static IdlingResource getResource() {
+    return resource;
+  }
+
+  private static IdlingResource resource;
+
   @NonNull
   public static OkHttpClient get(App app) {
-    return new OkHttpClient.Builder()
-        .addInterceptor(new DelayInterceptor(app.delayProvider()))
+    OkHttpClient okHttpClient = new Builder()
         .build();
+    resource = OkHttp3IdlingResource.create("OkHttp", okHttpClient);
+    return okHttpClient;
   }
 
   @NonNull
