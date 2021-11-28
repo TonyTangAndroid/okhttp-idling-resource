1a6cbe162f1a12491d5575d4e1c3bfb813af5499
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 00:04:33 -0800

    Make the unit test fails to demonstrate the registeration of IdleResource should come before the unit test is to be executed

--- app/src/main/java/com/demo/app/OkhttpProvider.java
+++ app/src/main/java/com/demo/app/OkhttpProvider.java
@@ -1,10 +1,8 @@
 package com.demo.app;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.test.espresso.IdlingResource;
 import com.jakewharton.espresso.OkHttp3IdlingResource;
-import com.simplemented.okdelay.DelayInterceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.OkHttpClient.Builder;
 import retrofit2.Retrofit;
@@ -13,18 +11,29 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OkhttpProvider {
 
+  private static OkHttpClient okHttpClient;
+
   public static IdlingResource getResource() {
+    if (resource == null) {
+      init();
+    }
     return resource;
   }
 
   private static IdlingResource resource;
 
   @NonNull
-  public static OkHttpClient get(App app) {
-    OkHttpClient okHttpClient = new Builder()
-        .build();
-    resource = OkHttp3IdlingResource.create("OkHttp", okHttpClient);
+  public static OkHttpClient get() {
+    if (okHttpClient != null) {
+      return okHttpClient;
+    }
+    init();
     return okHttpClient;
+  }
+
+  private static void init() {
+    okHttpClient = new Builder().build();
+    resource = OkHttp3IdlingResource.create("OkHttp", okHttpClient);
   }
 
   @NonNull
