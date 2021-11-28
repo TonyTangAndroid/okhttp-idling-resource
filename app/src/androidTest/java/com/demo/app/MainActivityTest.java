31f1adaea8f7f5178326e5771be23e7e60cd1a4c
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 00:49:07 -0800

    Use Baristsa to test out the code.

--- app/src/androidTest/java/com/demo/app/MainActivityTest.java
+++ app/src/androidTest/java/com/demo/app/MainActivityTest.java
@@ -5,9 +5,12 @@ import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertContains;
+import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
+import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotExist;
+import static com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn;
 
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -39,17 +42,13 @@ public class MainActivityTest {
   public void whenCallRequestIsClickedAndNetworkReturned_shouldShowResult() {
 
     // by default, it is idle
-    ViewInteraction state = onView(withText("Idle"));
-    state.check(matches(isDisplayed()));
+    assertDisplayed("Idle");
 
-    onView(withText("CALL REQUEST")).perform(ViewActions.click());
+    clickOn(R.id.activity_main_call_request);
 
     // With IdlingRegistry applied, we will never check the intermediate state.
-    ViewInteraction loading = onView(withText("Loading"));
-    loading.check(doesNotExist());
-
-    // after 6 seconds, it is success
-    ViewInteraction success = onView(withText("Success"));
-    success.check(matches(isDisplayed()));
+    assertNotExist("Loading");
+    assertDisplayed("Success");
+    assertContains("Request took");
   }
 }
